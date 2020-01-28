fbe1caa3f5342abeb674de8d76d0c6a795d5538f
nbhaohao <nbhaohao@163.com>
2020-01-28 21:01:18 +0800

    init: webpack 打包成功

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./lib/index.tsx"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/lib"),
+    library: "reactUI",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  }
+};
