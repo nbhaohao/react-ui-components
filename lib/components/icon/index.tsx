68b2bb08815e62ba96ccb392d78475bd343fe986
nbhaohao <nbhaohao@163.com>
2020-02-16 13:45:13 +0800

    chore: 调整 index.tsx 位置

--- lib/components/icon/index.tsx
+++ lib/components/icon/index.tsx
@@ -7,7 +7,7 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
-const Index: React.FC<IconProps> = ({ name, className, ...svgAttributes }) => {
+const Icon: React.FC<IconProps> = ({ name, className, ...svgAttributes }) => {
   return (
     <svg className={classes("react-ui-icon", className)} {...svgAttributes}>
       <use xlinkHref={`#${name}`} />
@@ -15,4 +15,4 @@ const Index: React.FC<IconProps> = ({ name, className, ...svgAttributes }) => {
   );
 };
 
-export default Index;
+export default Icon;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    index: "./lib/index.tsx"
+    index: "./index.tsx"
   },
   output: {
     path: path.resolve(__dirname, "dist"),
